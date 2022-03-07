9b075936ed7778937dce80ac84036b6fec08b00f
asiful087 <asiful087@gmail.com>
2022-03-07 09:04:46 +0600

    word search 2 done , though having tle error and plz dont forget to self.prop = par not : again

--- 212.word-search-ii.py
+++ 212.word-search-ii.py
@@ -6,70 +6,85 @@
 
 # @lc code=start
 class TrieNode:
-    def __init__(self) -> None:
-        self.childrens = {}
-        self.is_end: False
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
     
-    def addNode(self, word):
+    def addWord(self, word):
         cur = self
         for c in  word:
-            if c not in cur.childrens:
-                cur.childrens[c] = TrieNode()
-            cur = cur.childrens[c]
-        cur.is_end = True
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isWord = True
 
 
 
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-
-        R = len(board)
-        C = len(board[0])
-
-        vis = [[False for j in range(C)] for i in range(R)]
-
-        path = set()
-
-        def isValid(row, col, i, word):
-            
-            if (row < 0 or col < 0 or row >= R or col >= C):
-                return False
-
-            if not (word[i] == board[row][col]):
-                return False
+        # 1. have to add all the words to a prefix tree
+        # 2. dfs to the board and check if any word exists
         
-            if (vis[row][col]):
-                return False
-
-            return True
-
-
-        def dfs(row, col, node, word):
-
-            if i == len(word):
-                return True
-
-            if not isValid(row,col,i,word):
-                return False
-
-            vis[row][col] = True
-
-            res = (dfs(row-1, col, i+1,word) or dfs(row, col+1, i+1,word) or dfs(row+1, col, i+1,word) or dfs(row, col-1, i+1,word))
-
-            vis[row][col] = False
-
-            return res
-
-        new_list = []
-
+        # adding all word to a perfix tree
+        root = TrieNode()
         for w in words:
-            for r in range(R):
-                for c in range(C):
-                    if dfs(r,c,0,w): new_list.append(w)
+            root.addWord(w)
+
+        # print(vars(root))
+        ROWS , COLS = len(board) , len(board[0])
+
+        # vis = [[False for j in range(C)] for i in range(R)]
+        res, visit= set(), set()
+
+        # def isValid(row, col, node):
+            
+        #     if (row < 0 or col < 0 or row >= R or col >= C):
+        #         return False
+
+        #     if  board[row][col] in node.childrens:
+        #         return False
+        
+        #     if (vis[row][col]):
+        #         return False
+
+        #     return True
 
 
-        return list(set(new_list))
+        def dfs(r, c, node, word):
+
+
+            # if not isValid(row,col,node):
+            #     return 
+
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visit or board[r][c] not in node.children):
+                return 
+
+
+            # vis[row][col] = True
+            visit.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.add(word)
+
+
+            dfs(r-1, c, node, word)
+            dfs(r+1, c, node, word)
+            dfs(r, c-1, node, word)
+            dfs(r, c+1, node, word)
+
+            # vis[row][col] = False
+            visit.remove((r,c))
+
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c,root,"")
+
+
+        return list(res)
 
         
 # @lc code=end
